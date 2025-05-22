cff19488cb07823b39c29cd1d978117d78f2eba1
MercedesVega08 <168444159+MercedesVega08@users.noreply.github.com>
2025-05-22 19:34:36 -0300

    Más o menos se terminó el ejercicio opcional

--- additional/practica-5-opcional/opcional/src/main/java/Calculadora.java
+++ additional/practica-5-opcional/opcional/src/main/java/Calculadora.java
@@ -1,24 +1,28 @@
+import java.util.ArrayList;
+
 import list.MyArrayList;
-import java.util.Iterator; 
 
 public class Calculadora{
     
     private String operacion;
     private MyArrayList<String> historial;
     private int cantOperation;
+    private static int size;
 
     public Calculadora(){
         this.operacion = null;
-        historial = new MyArrayList<>();
+        historial = new MyArrayList<>(15);
         cantOperation = 0;
+        size = 0;
     }
 
     public int getCantOperation(){
         return cantOperation;
     }
 
-    public void add(String operacion){
-        this.operacion = operacion;
+    public void add(){
+        if(cantOperation == historial.size())
+            remove();
         historial.add(operacion);
         cantOperation++;
     }
@@ -28,18 +32,71 @@ public class Calculadora{
         cantOperation--;
     }
 
-    private int transformarInt(String number, String number2){
+    private int functionPower(int base, int potencia){
+        int result = base;
+        if(potencia == 0)
+            result = 1;
+        if(potencia == 1)
+            result *= potencia;
+        for(int i = 1; i < potencia; i++)
+            result *= base;
+       
+        return result;
+    }
+
+    private int transformarInt(String number){
         int number1 = 0;
-        char[] charNumber = new char[number.length()];
-        char[] charNumber2 = new char[number2.length()];
+        size = number.length();
+
+        for(int i = 0; i < size; i++)
+            number1 += ((number.charAt(i)-48)*functionPower(10, size-(i+1)));
         
-        number.chars();
-        return 0;
+        return number1;
+    }
+
+    private String transformarString(int number){
+        String number1 = "";
+        number1 += number;
+        return number1;
     }
 
     public String suma(String operation1, String operation2){
-        
-        
-        return null;
+        int add = transformarInt(operation1) + transformarInt(operation2);
+        operacion = transformarString(add);
+        return operacion;
     }
+
+    public String resta(String operation1, String operation2){
+        int sub = transformarInt(operation1) - transformarInt(operation2);
+        operacion = transformarString(sub);
+        return operacion;
+    }
+
+    public String division(String operacion1, String operacion2){
+        int div = transformarInt(operacion1) / transformarInt(operacion2);
+        operacion = transformarString(div);
+        return operacion;
+    }
+
+    public String multiplicacion(String operacion1, String operacion2){
+        int mult = transformarInt(operacion1) * transformarInt(operacion2);
+        operacion = transformarString(mult);
+        return operacion;
+    }
+
+    public String potencia(String base, String potencia){
+        int power = functionPower(transformarInt(base), transformarInt(potencia));
+        operacion = transformarString(power);
+        return operacion;
+    }
+
+    public String toString(){
+        String history = "[";
+        String history2 = "";
+        for(int i = 0; i < historial.size(); i++)
+            history2 +=  ", ";
+        history += history2 + "]";
+        return history;
+    }
+   
 }
--- additional/practica-5-opcional/opcional/src/main/java/CalculadoraMain.java
+++ additional/practica-5-opcional/opcional/src/main/java/CalculadoraMain.java
@@ -1,3 +1,18 @@
 public class CalculadoraMain {
     
+    public static void main(String[] args){
+        Calculadora calcu = new Calculadora();
+        System.out.println("Suma: "+ calcu.suma("34", "8"));
+        calcu.add();
+        System.out.println("Resta: " + calcu.resta("34", "4"));
+        calcu.add();
+        System.out.println("Multiplicacion: " + calcu.multiplicacion("34", "4"));
+        calcu.add();
+        System.out.println("Division: " + calcu.division("34", "4"));
+        calcu.add();
+        System.out.println("Potencia: " + calcu.potencia("34", "4"));
+        calcu.add();
+        System.out.println("Historial: " + calcu.toString());
+    
+    }
 }
